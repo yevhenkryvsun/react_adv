9fca20841ba887ba2b5540fa6cb7d108cb6818e9
Yevhen Kryvsun <yevhen@MacBook-Pro.local>
2018-03-20 11:53:57 +0200

    add packages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Root from './components/Root'
+import store from './redux'
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'react-router-redux'
+import history from './history'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <Root/>
+            </ConnectedRouter>
+        </Provider>
     );
   }
 }
